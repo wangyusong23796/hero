be9881c9ef91a148bff78407aed5e620d10f7f7d
wangyusong <admin@wangyusong.com>
2015-04-25 20:01:01 +0800

    'update'

--- App/index/controllers/IndexController.php
+++ App/index/controllers/IndexController.php
@@ -1,11 +1,19 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-
+require 'BaseController.php';
 
 
 class IndexController extends CI_Controller {
-
+	
+	
+	public function __construct()
+	{
+		parent::__construct();
+		
+	}
+	
+	
 	public function index()
 	{
 		die('hi');
